e1a485ad6719951b7816445415b16a69b89c192d
Nikolai Åkerholt <nikolai_aakk@hotmail.com>
2019-11-18 19:24:46 +0100

    Now the data is extracted as intended into output/*.json files when done parsing demo data

--- dataset/inventory/inventory.go
+++ dataset/inventory/inventory.go
@@ -1,16 +1,29 @@
 package inventory
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/markus-wa/demoinfocs-golang/common"
+	"io/ioutil"
+	"strconv"
 )
-//TODO: Json marshal/unmarshaling for writing to file
-
 
 type Equipment struct{
 	Name string
 	Count int
 }
 
+// TODO: We could possibly also have info like what map etc
+type RoundInfo struct{
+	CTEquipment []Equipment
+	CTSideTotalWorth int
+	TEquipment []Equipment
+	TSideTotalWorth int
+	TerroristsWon bool
+}
+
+var equipmentCount int
+var resultFiles int
 var equipmentList []Equipment
 var equipmentKeys map[common.EquipmentElement]int // To be able to easier insert weapons correctly when retrieving weapons
 
@@ -18,8 +31,8 @@ var equipmentKeys map[common.EquipmentElement]int // To be able to easier insert
 // in regards to making the array indices the same for every equipment type
 // Maps seem to have random order? Maybe need to hardcode this instead
 func Init(){
+	resultFiles = 0
 	initEquipment()
-	initEquipmentKeys()
 }
 
 func GetEquipmentList() []Equipment{
@@ -29,160 +42,79 @@ func GetEquipmentList() []Equipment{
 	return returnList
 }
 
+func WriteToJSON(gameInfo []RoundInfo, fileName string){
+	file, _ := json.MarshalIndent(gameInfo, "", " ")
+	resultFiles += 1
+	filePath := "output/results_" + strconv.Itoa(resultFiles) + "_" + fileName + ".json"
+	_ = ioutil.WriteFile(filePath, file, 0644)
+
+	fmt.Printf("Wrote output to " + filePath + "\n")
+}
+
 func AddToList(list []Equipment, e common.EquipmentElement) []Equipment{
 	list[equipmentKeys[e]].Count += 1
 	return list
 }
 
 // Using a hardcoded array because we don't want the data to lie randomly everytime like it does with a map
+
 func initEquipment(){
-	newEquipment := Equipment{ Name: "AK-47", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "AUG", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "AWP", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "PP-Bizon", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "C4", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Desert Eagle", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Decoy Grenade", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Dual Berettas", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "FAMAS", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Five-SeveN", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Flashbang", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "G3SG1", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Galil AR", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Glock-18", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "HE Grenade", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "P2000", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Incendiary Grenade", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "M249", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "M4A4", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "MAC-10", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "MAG-7", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Molotov", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "MP7", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "MP5-SD", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "MP9", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Negev", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Nova", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "p250", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "P90", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Sawed-Off", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "SCAR-20", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "SG 553", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Smoke Grenade", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "SSG 08", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Zeus x27", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Tec-9", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "UMP-45", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "XM1014", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "M4A1", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "CZ75 Auto", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "USP-S", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "World", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "R8 Revolver", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Kevlar Vest", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Kevlar + Helmet", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Defuse Kit", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "Knife", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
-	newEquipment = Equipment{ Name: "UNKNOWN", Count: 0,}
-	equipmentList = append(equipmentList, newEquipment)
+	equipmentKeys = make(map[common.EquipmentElement]int)
+	AddEquipment("AK-47", common.EqAK47)
+	AddEquipment("AUG", common.EqAUG)
+	AddEquipment("AWP", common.EqAWP)
+	AddEquipment("PP-Bizon", common.EqBizon)
+	AddEquipment("C4", common.EqBomb)
+	AddEquipment("Desert Eagle", common.EqDeagle)
+	AddEquipment("Decoy Grenade", common.EqDecoy)
+	AddEquipment("Dual Berettas", common.EqDualBerettas)
+	AddEquipment("FAMAS", common.EqFamas)
+	AddEquipment("Five-SeveN", common.EqFiveSeven)
+	AddEquipment("Flashbang", common.EqFlash)
+	AddEquipment("G3SG1", common.EqG3SG1)
+	AddEquipment("Galil AR", common.EqGalil)
+	AddEquipment("Glock-18", common.EqGlock)
+	AddEquipment("HE Grenade", common.EqHE)
+	AddEquipment("P2000", common.EqP2000)
+	AddEquipment("Incendiary Grenade", common.EqIncendiary)
+	AddEquipment("M249", common.EqM249)
+	AddEquipment("M4A4", common.EqM4A4)
+	AddEquipment("MAC-10", common.EqMac10)
+	AddEquipment("MAG-7", common.EqSwag7)
+	AddEquipment("Molotov", common.EqMolotov)
+	AddEquipment("MP7", common.EqMP7)
+	AddEquipment("MP5-SD", common.EqMP5)
+	AddEquipment("MP9", common.EqMP9)
+	AddEquipment("Negev", common.EqNegev)
+	AddEquipment("Nova", common.EqNova)
+	AddEquipment("p250", common.EqP250)
+	AddEquipment("P90", common.EqP90)
+	AddEquipment("Sawed-Off", common.EqSawedOff)
+	AddEquipment("SCAR-20", common.EqScar20)
+	AddEquipment("SG 553", common.EqSG553)
+	AddEquipment("Smoke Grenade", common.EqSmoke)
+	AddEquipment("SSG 08", common.EqScout)
+	AddEquipment("Zeus x27", common.EqZeus)
+	AddEquipment("Tec-9", common.EqTec9)
+	AddEquipment("UMP-45", common.EqUMP)
+	AddEquipment("XM1014", common.EqXM1014)
+	AddEquipment("M4A1", common.EqM4A1)
+	AddEquipment("CZ75 Auto", common.EqCZ)
+	AddEquipment("USP-S", common.EqUSP)
+	AddEquipment("World", common.EqWorld)
+	AddEquipment("R8 Revolver", common.EqRevolver)
+	AddEquipment("Kevlar Vest", common.EqKevlar)
+	AddEquipment("Kevlar + Helmet", common.EqHelmet)
+	AddEquipment("Defuse Kit", common.EqDefuseKit)
+	AddEquipment("Knife", common.EqKnife)
+	AddEquipment("UNKNOWN", common.EqUnknown)
 }
 
-func initEquipmentKeys(){
-	equipmentKeys = make(map[common.EquipmentElement]int)
-	equipmentKeys[common.EqAK47] 			= 0
-	equipmentKeys[common.EqAUG] 			= 1
-	equipmentKeys[common.EqAWP] 			= 2
-	equipmentKeys[common.EqBizon] 			= 3
-	equipmentKeys[common.EqBomb] 			= 4
-	equipmentKeys[common.EqDeagle] 			= 5
-	equipmentKeys[common.EqDecoy] 			= 6
-	equipmentKeys[common.EqDualBerettas] 	= 7
-	equipmentKeys[common.EqFamas] 			= 8
-	equipmentKeys[common.EqFiveSeven] 		= 9
-	equipmentKeys[common.EqFlash] 			= 10
-	equipmentKeys[common.EqG3SG1] 			= 11
-	equipmentKeys[common.EqGalil] 			= 12
-	equipmentKeys[common.EqGlock] 			= 13
-	equipmentKeys[common.EqHE] 				= 14
-	equipmentKeys[common.EqP2000] 			= 15
-	equipmentKeys[common.EqIncendiary] 		= 16
-	equipmentKeys[common.EqM249] 			= 17
-	equipmentKeys[common.EqM4A4] 			= 18
-	equipmentKeys[common.EqMac10] 			= 19
-	equipmentKeys[common.EqSwag7] 			= 20
-	equipmentKeys[common.EqMolotov] 		= 21
-	equipmentKeys[common.EqMP7] 			= 22
-	equipmentKeys[common.EqMP5] 			= 23
-	equipmentKeys[common.EqMP9] 			= 24
-	equipmentKeys[common.EqNegev] 			= 25
-	equipmentKeys[common.EqNova] 			= 26
-	equipmentKeys[common.EqP250] 			= 27
-	equipmentKeys[common.EqP90] 			= 28
-	equipmentKeys[common.EqSawedOff] 		= 29
-	equipmentKeys[common.EqScar20] 			= 30
-	equipmentKeys[common.EqSG553] 			= 31
-	equipmentKeys[common.EqSmoke] 			= 32
-	equipmentKeys[common.EqScout] 			= 33
-	equipmentKeys[common.EqZeus] 			= 34
-	equipmentKeys[common.EqTec9] 			= 35
-	equipmentKeys[common.EqUMP] 			= 36
-	equipmentKeys[common.EqXM1014] 			= 37
-	equipmentKeys[common.EqM4A1] 			= 38
-	equipmentKeys[common.EqCZ] 				= 39
-	equipmentKeys[common.EqUSP] 			= 40
-	equipmentKeys[common.EqWorld] 			= 41
-	equipmentKeys[common.EqRevolver] 		= 42
-	equipmentKeys[common.EqKevlar] 			= 43
-	equipmentKeys[common.EqHelmet] 			= 44
-	equipmentKeys[common.EqDefuseKit] 		= 45
-	equipmentKeys[common.EqKnife] 			= 46
-	equipmentKeys[common.EqUnknown] 		= 47
+func AddEquipment(name string, elem common.EquipmentElement){
+	newEquipment := Equipment{ Name: name, Count: 0,}
+	equipmentList = append(equipmentList, newEquipment)
+	equipmentKeys[elem] = equipmentCount
+	equipmentCount += 1
 }
+
 
--- dataset/main.go
+++ dataset/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
+	dem "github.com/markus-wa/demoinfocs-golang"
+	"github.com/markus-wa/demoinfocs-golang/common"
+	events "github.com/markus-wa/demoinfocs-golang/events"
 	"github.com/mortenoj/deep-learning-project/dataset/inventory"
+	"github.com/mortenoj/deep-learning-project/dataset/utils"
+	"github.com/mortenoj/deep-learning-project/dataset/webscraper"
+	"github.com/sirupsen/logrus"
 	"os"
 	"path/filepath"
-
-	dem "github.com/markus-wa/demoinfocs-golang"
-	events "github.com/markus-wa/demoinfocs-golang/events"
-	"github.com/mortenoj/deep-learning-project/dataset/utils"
 )
 
 // Run like this: go run print_kills.go
 func main() {
-	/*
-	var err error
+	//var err error
+
+	inventory.Init()
 
 	links, err := webscraper.GetLinks(0, 1)
 	if err != nil {
@@ -29,14 +31,9 @@ func main() {
 			panic(err)
 		}
 	}
-	*/
 
-	inventory.Init()
-
-
-
-
-	extractDataset("demos/fnatic-vs-vitality-m1-nuke.dem")
+	// Debug TODO: Remove
+	//extractDataset("demos/fnatic-vs-vitality-m1-nuke.dem")
 
 }
 
@@ -79,8 +76,12 @@ func extractDataset(filePath string) error {
 
 	p := dem.NewParser(f)
 
+	var gameInfo []inventory.RoundInfo
+	var roundInfo inventory.RoundInfo
+
 	tScoreRoundStart := 0
 	ctScoreRoundStart := 0
+
 	// Register handler on kill events
 	//p.RegisterEventHandler(func(e events.Kill) {
 	//if e.PenetratedObjects > 0 && !e.IsHeadshot {
@@ -105,12 +106,18 @@ func extractDataset(filePath string) error {
 		ctEquipment := inventory.GetEquipmentList()
 		tEquipment := inventory.GetEquipmentList()
 
-		// TODO: Possibly make a struct of all weapons for putting this into a JSON that matches the JSON output from python
 		for _, player := range ctPlayers {
-			//fmt.Printf("%v \n", player.Weapons())
-			//fmt.Printf("%v \n", player.RawWeapons)
 			for _, weapon := range player.Weapons() {
 				ctEquipment = inventory.AddToList(ctEquipment, weapon.Weapon)
+			}
+			if player.HasDefuseKit {
+				ctEquipment = inventory.AddToList(ctEquipment, common.EqDefuseKit)
+			}
+			if player.Armor > 0{
+				ctEquipment = inventory.AddToList(ctEquipment, common.EqKevlar)
+			}
+			if player.HasHelmet{
+				ctEquipment = inventory.AddToList(ctEquipment, common.EqHelmet)
 			}
 		}
 
@@ -118,23 +125,18 @@ func extractDataset(filePath string) error {
 			for _, weapon := range player.Weapons() {
 				tEquipment = inventory.AddToList(tEquipment, weapon.Weapon)
 			}
+			if player.Armor > 0{
+				tEquipment = inventory.AddToList(tEquipment, common.EqKevlar)
+			}
+			if player.HasHelmet{
+				tEquipment = inventory.AddToList(tEquipment, common.EqHelmet)
+			}
 		}
 
-		for e, k := range ctEquipment{
-			fmt.Printf("CT Equipment: %d %s %d \n", e, k.Name, k.Count)
-		}
-		for e, k := range tEquipment{
-			fmt.Printf("T Equipment: %d %s %d \n", e, k.Name, k.Count)
-		}
-
-		//ctEquipment := ct.FreezeTimeEndEquipmentValue()
-		//tEquipment := t.FreezeTimeEndEquipmentValue()
-
-		//fmt.Printf("CT Equipment value: %d\n", ctEquipment)
-		//fmt.Printf("CT Equipment value: %d\n", ct.CurrentEquipmentValue())
-		//fmt.Printf("T Equipment value: %d\n", tEquipment)
-		//fmt.Printf("T Equipment value: %d\n", t.CurrentEquipmentValue())
-		//fmt.Printf("CT equipment value: %d\n", t.CurrentEquipmentValue())
+		roundInfo.CTEquipment = ctEquipment
+		roundInfo.CTSideTotalWorth = ct.CurrentEquipmentValue()
+		roundInfo.TEquipment = tEquipment
+		roundInfo.TSideTotalWorth = t.CurrentEquipmentValue()
 	})
 
 	// RoundEndOffical is the most accurate and gives what we want, but it does not give the result of the last round
@@ -145,21 +147,19 @@ func extractDataset(filePath string) error {
 		if gs.IsMatchStarted() != true {
 			return
 		}
-		ct := gs.TeamCounterTerrorists()
 		t := gs.TeamTerrorists()
 
 		if t.Score > tScoreRoundStart {
-			fmt.Printf("Terrorists won the round, score is now: T:%d - CT: %d\n", t.Score, ct.Score)
+			roundInfo.TerroristsWon = true
 		} else {
-			fmt.Printf("Counter-Terrorists won the round, score is now: T:%d - CT: %d\n", t.Score, ct.Score)
+			roundInfo.TerroristsWon = false
 		}
+
+		gameInfo = append(gameInfo, roundInfo)
 
 		// Save round winner in a array or something e.g. 1 = T win, 0 = CT win
 		// Save these to file when everything is done parsing
 	})
-
-	// TODO: Catch event for when game is totally over?
-	//  Or no need as that will be when parsing is done?
 
 	// Parse to end
 	err = p.ParseToEnd()
@@ -167,5 +167,20 @@ func extractDataset(filePath string) error {
 		return nil
 	}
 
+	// Gathering the round winner of the last round as it is not collected in events.RoundEndOfficial
+	gs := p.GameState()
+	ct := gs.TeamCounterTerrorists()
+	t := gs.TeamTerrorists()
+	if t.Score > ct.Score{
+		roundInfo.TerroristsWon = true
+	} else {
+		roundInfo.TerroristsWon = false
+	}
+	gameInfo = append(gameInfo, roundInfo)
+
+	// TODO: Caused some errors, need to be looked into, not vital though
+	// Writing to file with a unique name identifier of the demo name
+	//fileName := strings.Split(filePath, "/")
+	inventory.WriteToJSON(gameInfo, "test")
 	return nil
 }
